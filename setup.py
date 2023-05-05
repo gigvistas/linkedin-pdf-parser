7ef0660114838315b352ad97f659668f2f53d2f3
Seshi Kiran <seshi.kiran@gigvistas.com>
2023-05-05 15:17:38 +0530

    modified version_scheme in setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@
 """
 from setuptools import setup
 
+from setuptools import setup
+
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
-    except:  # noqa
+        setup(use_scm_version=True)
+    except Exception as e:
+        print("\n\nAn error occurred while building the project:", e)
         print(
-            "\n\nAn error occurred while building the project, "
-            "please ensure you have the most updated version of setuptools, "
-            "setuptools_scm and wheel with:\n"
+            "\n\nPlease ensure you have the most updated version of setuptools, setuptools_scm, and wheel with:\n"
             "   pip install -U setuptools setuptools_scm wheel\n\n"
         )
         raise
